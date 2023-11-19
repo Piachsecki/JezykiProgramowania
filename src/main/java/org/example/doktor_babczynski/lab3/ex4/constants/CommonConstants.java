da284da413445cae3f72af067abe15e73f933353
piachsecki <k.r.piasecki2@gmail.com>
2023-11-19 22:17:52 +0100

    [LAB3] calculator done

---
+++ src/main/java/org/example/doktor_babczynski/lab3/ex4/constants/CommonConstants.java
@@ -0,0 +1,18 @@
+package org.example.doktor_babczynski.lab3.ex4.constants;
+
+public class CommonConstants {
+    public static final String APP_NAME = "CALCULATOR";
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 600;
+
+    public static final int TEXT_FIELD_LENGTH = 10;
+    public static final int TEXT_FIELD_SPRING_LAYOUT_NORTH_PAD = 30;
+    public static final int TEXT_FIELD_SPRING_LAYOUT_WEST_PAD = 33;
+    public static final int BUTTON_COLUMN_COUNT = 4;
+    public static final int BUTTON_ROW_COUNT = 4;
+    public static final int BUTTON_COUNT = 16;
+    public static final int BUTTON_SPRING_LAYOUT_NORTH_PAD =150 ;
+    public static final int BUTTON_SPRING_LAYOUT_WEST_PAD = 33;
+    public static final int BUTTON_VGAP = 25;
+    public static final int BUTTON_HGAP = 25;
+}
